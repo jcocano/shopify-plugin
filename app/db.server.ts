5277538717817ab9f7e03f4cbdc2cf31cae3ab51
Jesus Cocaño <jesus.cocano@yugalabs.io>
2025-04-16 09:08:06 -0600

    fix

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,15 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-  var prismaGlobal: PrismaClient | undefined;
+  var prismaGlobal: PrismaClient;
 }
-
-const prisma = global.prismaGlobal ?? new PrismaClient({
-  log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-});
 
 if (process.env.NODE_ENV !== "production") {
-  global.prismaGlobal = prisma;
+  if (!global.prismaGlobal) {
+    global.prismaGlobal = new PrismaClient();
+  }
 }
+
+const prisma = global.prismaGlobal ?? new PrismaClient();
 
 export default prisma;
